fec19b4dc3f0c573ab11d2039bd741adb6a18a37
MSP Coder <mspcoder@example.com>
2023-07-19 23:48:36 +0000

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:"",
+                                  email: "user@invalid",
+                                  password: "foo",
+                                  password_confirmation: "Bar"}}
+    end
+    assert_template 'users/new'
+  end
+  
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: {name: "Example User",
+                                  email: "Example@example.com",
+                                  password: "Foobar",
+                                  password_confirmation: "Foobar"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+  
+end
